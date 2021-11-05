722dae80174abaac17f1537a7327061f12e042dc
redondoM20 <marcos.robred@educa.jcyl.es>
2021-11-05 20:08:08 +0100

    Exercise alerts

--- app/src/main/java/com/mrredondo/pmdm/ut2/exerciseAlerts/presentation/AlertsActivity.kt
+++ app/src/main/java/com/mrredondo/pmdm/ut2/exerciseAlerts/presentation/AlertsActivity.kt
@@ -13,7 +13,6 @@ import com.mrredondo.pmdm.ut2.exerciseAlerts.domain.GetAlertsUseCase
 class AlertsActivity : AppCompatActivity() {
 
     private val TAG: String = AlertsActivity::class.java.name
-
     private lateinit var viewBinding: ActivityAlertsBinding
 
     private val viewModel: AlertsViewModel =
@@ -21,32 +20,21 @@ class AlertsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alerts)
+        val layoutInflater = LayoutInflater.from(this)
+        viewBinding = ActivityAlertsBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
         setupView()
         render()
     }
 
     private fun setupView() {
-        val layoutInflater = LayoutInflater.from(this)
-        viewBinding = ActivityAlertsBinding.inflate(layoutInflater)
+
 
         imageIconAlert()
-        imageIconAlert1()
-        imageIconAlert2()
     }
 
     private fun imageIconAlert() {
         val image: AppCompatImageView = findViewById(R.id.iconAlert)
-        image.setImageResource(R.drawable.ic_launcher_foreground)
-    }
-
-    private fun imageIconAlert1() {
-        val image: AppCompatImageView = findViewById(R.id.iconAlert1)
-        image.setImageResource(R.drawable.ic_launcher_foreground)
-    }
-
-    private fun imageIconAlert2() {
-        val image: AppCompatImageView = findViewById(R.id.iconAlert2)
         image.setImageResource(R.drawable.ic_launcher_foreground)
     }
 
@@ -57,14 +45,6 @@ class AlertsActivity : AppCompatActivity() {
         viewBinding.titleAlert.text = alertModel[0].title
         viewBinding.dateAlert.text = alertModel[0].datePublished
         viewBinding.resumeAlert.text = alertModel[0].summary
-
-        /*viewBinding.titleAlert1.text = alertModel[1].title
-        viewBinding.dateAlert1.text = alertModel[1].datePublished
-        viewBinding.resumeAlert1.text = alertModel[1].summary
-
-        viewBinding.titleAlert2.text = alertModel[2].title
-        viewBinding.dateAlert2.text = alertModel[2].datePublished
-        viewBinding.resumeAlert2.text = alertModel[2].summary*/
 
     }
 }
